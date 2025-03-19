af917f404bb826334a80ebfae2834df774f511cc
Deep Sengar <128704275+DeepSengar1@users.noreply.github.com>
2025-03-20 00:20:51 +0530

    Updated auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://climbing-cattle-3.clerk.accounts.dev/",
+      domain: "https://golden-silkworm-55.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
